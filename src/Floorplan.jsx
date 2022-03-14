909337490de25d65d1c2d5ca763582405ccbd7a6
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-14 15:27:04 -0400

    add kitchen to floorplan

---
+++ src/Floorplan.jsx
@@ -0,0 +1,14 @@
+import Kitchen from "./Kitchen"
+
+function Floorplan (pros){
+  return(
+    <>
+    <h1>This is a house</h1>
+    <h2>
+      < Kitchen/> 
+    </h2>
+    </>
+  )
+}
+
+export default Floorplan
---
+++ src/Kitchen.jsx
@@ -0,0 +1,12 @@
+const Kitchen = (props) => {
+  return (  
+    <>
+    <div>
+
+      <h3>This is a kitchen</h3>
+    </div>
+    </>
+  );
+}
+ 
+export default Kitchen;
